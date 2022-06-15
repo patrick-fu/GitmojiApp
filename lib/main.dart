7c31b2b19475e4de53b0bc2142ad47918d2b5f93
Patrick Fu <paaatrickfu@gmail.com>
2022-06-16 01:02:58 +0800

    🚩 Customize window style, set minimum window size and remove the title bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,39 @@
+import 'dart:io';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:gitmojiapp/home_page.dart';
 import 'package:gitmojiapp/models/gitmoji_view_model.dart';
+import 'package:window_manager/window_manager.dart';
 
-void main() {
+Future<void> _ensureInitialized() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (!kIsWeb && (Platform.isMacOS || Platform.isWindows || Platform.isLinux)) {
+    await windowManager.ensureInitialized();
+    setupWindow();
+  }
+}
+
+void setupWindow() {
+  Size minSize = const Size(350, 450);
+  windowManager.setMinimumSize(minSize);
+  WindowOptions windowOptions = WindowOptions(
+    size: minSize,
+    backgroundColor: Colors.transparent,
+    skipTaskbar: false,
+    titleBarStyle: TitleBarStyle.hidden,
+  );
+
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.setAsFrameless();
+    await windowManager.setTitleBarStyle(TitleBarStyle.hidden);
+    await windowManager.show();
+    await windowManager.focus();
+  });
+}
+
+void main() async {
+  await _ensureInitialized();
   runApp(const MyApp());
 }
 
