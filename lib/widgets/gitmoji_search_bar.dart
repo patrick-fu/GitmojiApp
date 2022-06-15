a6608344dc2a352e5711dfc84b448792e12ac52e
Patrick Fu <paaatrickfu@gmail.com>
2022-06-16 01:05:43 +0800

    🚩 Add a clear text button for the search bar text field

--- lib/widgets/gitmoji_search_bar.dart
+++ lib/widgets/gitmoji_search_bar.dart
@@ -3,8 +3,15 @@ import 'package:gitmojiapp/settings_page.dart';
 import 'package:provider/provider.dart';
 import 'package:gitmojiapp/models/gitmoji_view_model.dart';
 
-class GitmojiSearchBar extends StatelessWidget {
+class GitmojiSearchBar extends StatefulWidget {
   const GitmojiSearchBar({Key? key}) : super(key: key);
+
+  @override
+  State<GitmojiSearchBar> createState() => _GitmojiSearchBarState();
+}
+
+class _GitmojiSearchBarState extends State<GitmojiSearchBar> {
+  final TextEditingController _searchTextController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -20,42 +27,55 @@ class GitmojiSearchBar extends StatelessWidget {
           borderRadius: BorderRadius.circular(20)),
       child: Row(
         children: [
-          const Text(
-            '🔍',
-            style: TextStyle(fontSize: 24),
-          ),
-          const SizedBox(
-            width: 8,
-          ),
-          Expanded(
-            child: TextField(
-              onChanged: (value) {
-                var gitmojiViewModel = context.read<GitmojiViewModel>();
-                gitmojiViewModel.filterText = value;
-              },
-              decoration: const InputDecoration(
-                border: InputBorder.none,
-                hintText: 'Search...',
-              ),
-            ),
-          ),
-          IconButton(
-              icon: const Icon(Icons.settings),
-              onPressed: () {
-                final model =
-                    Provider.of<GitmojiViewModel>(context, listen: false);
-                showModalBottomSheet(
-                    context: context,
-                    builder: (context) {
-                      return ListenableProvider.value(
-                          value: model,
-                          child: SettingsPage(onDismiss: () {
-                            Navigator.of(context).pop();
-                          }));
-                    });
-              })
+          const Text('🔍', style: TextStyle(fontSize: 24)),
+          const SizedBox(width: 8),
+          Expanded(child: inputTextField(context)),
+          clearTextButton(context),
+          settingsButton(context)
         ],
       ),
     );
+  }
+
+  Widget inputTextField(BuildContext context) {
+    return TextField(
+      controller: _searchTextController,
+      onChanged: (value) {
+        var gitmojiViewModel = context.read<GitmojiViewModel>();
+        gitmojiViewModel.filterText = value;
+      },
+      decoration: const InputDecoration(
+        border: InputBorder.none,
+        hintText: 'Search...',
+      ),
+    );
+  }
+
+  Widget clearTextButton(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.clear),
+      onPressed: () {
+        _searchTextController.clear();
+        var gitmojiViewModel = context.read<GitmojiViewModel>();
+        gitmojiViewModel.filterText = '';
+      },
+    );
+  }
+
+  Widget settingsButton(BuildContext context) {
+    return IconButton(
+        icon: const Icon(Icons.settings),
+        onPressed: () {
+          final model = Provider.of<GitmojiViewModel>(context, listen: false);
+          showModalBottomSheet(
+              context: context,
+              builder: (context) {
+                return ListenableProvider.value(
+                    value: model,
+                    child: SettingsPage(onDismiss: () {
+                      Navigator.of(context).pop();
+                    }));
+              });
+        });
   }
 }
