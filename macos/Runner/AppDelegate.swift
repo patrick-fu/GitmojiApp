dcc361889bfd6cfec33eafe2616c120dd8a737db
Patrick Fu <paaatrickfu@gmail.com>
2022-07-08 02:33:12 +0800

    🐛 Fix the issue that the app will be terminated when calling window manager's "hide" method

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -4,6 +4,6 @@ import FlutterMacOS
 @NSApplicationMain
 class AppDelegate: FlutterAppDelegate {
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-    return true
+    return false // To use window_manager's "hide" method
   }
 }
